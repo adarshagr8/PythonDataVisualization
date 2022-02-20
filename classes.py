6004169cf0f02e81424338c9f825b300daf371b1
Adarsh <adarshagr8@gmail.com>
2022-02-21 00:04:35 +0530

    Initial classes

---
+++ classes.py
@@ -0,0 +1,35 @@
+class Item:
+    
+    def __init__(self, name, icon, value=0, color):
+        self.name = name
+        self.icon = icon
+        self.value = value
+        self.color = color
+
+class Board:
+    
+    self.items = []
+    def __init__():
+        pass
+    
+    def addItem(self, item):
+        self.items.append(item)
+    
+    def sortItems(self):
+        self.items.sort(key = lambda item: -item.value)
+    
+    def topX(self, x = 10):
+        self.sortItems()
+        x = min(x, len(self.items))
+        return self.Items[:x]
+    
+class State:
+    
+    def __init__(self):
+        self.board = Board()
+        self.date = Null
+        
+    def updateState(self, board, date):
+        self.prevBoard = self.board
+        self.board = board
+        self.date = date
